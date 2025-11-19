436ad4ba0c9fc086ceebc95f0987f9d58ce89ca1
Navnath Kadam <navnathkadam284@gmail.com>
2025-11-19 17:20:57 +0530

    refactor: streamline error handling and logging in createTaskApi function

--- client/src/api/createTaskApi.ts
+++ client/src/api/createTaskApi.ts
@@ -2,26 +2,29 @@ import { api } from "./globalApi";
 import type { CreateTaskReq } from "../types/createTaskType";
 
 const createTaskApi = async (data: CreateTaskReq): Promise<any> => {
+  try {
+    console.log("createTaskApi called with data:", data);
 
-  console.log("createTaskApi called with data:", data);
+    // Do NOT send a custom _id: server expects a Mongo ObjectID and will generate one
+    const response: any = await api.post(
+      `/users/${data.userId}/create-todo/${data.workspaceId}`,
+      {
+        task: data.text,
+        priority: data.priority,
+        userId: data.userId,
+        workspaceId: data.workspaceId,
+        done: false,
+      }
+    );
 
-  // Do NOT send a custom _id: server expects a Mongo ObjectID and will generate one
-  const response: any = await api.post(
-    `/users/${data.userId}/create-todo/${data.workspaceId}`,
-    {
-      task: data.text,
-      priority: data.priority,
-      userId: data.userId,
-      workspaceId: data.workspaceId,
-      done: false,
+    if (response.status < 200 || response.status >= 300) {
+      throw new Error(response.data.Error);
     }
-  );
 
-  if (response.status < 200 || response.status >= 300) {
-    throw new Error(response.data.Error);
+    return response.data;
+  } catch (error) {
+    console.log("Error in createTaskApi:", error);
   }
-
-  return response.data;
 };
 
 export default createTaskApi;
